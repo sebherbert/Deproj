d77f866ef22df44eb49c3fc04cd40ed7bd7e63e8
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2020-07-04 22:41:45 +0200

    Plot all ellipses from an epicell collection.

---
+++ src/plot_fit_ellipse.m
@@ -0,0 +1,24 @@
+function [ hf, hc, he ] = plot_fit_ellipse( epicells )
+%PLOT_FIT_ELLIPSE Summary of this function goes here
+
+    hf = figure( 'Position', [ 1204 20 600 500 ] );
+    hold on
+    axis equal
+    
+    n_obj = numel( epicells );
+    hc = NaN( n_obj, 1 );
+    he = NaN( n_obj, 1 );
+    
+    for i = 1 : n_obj
+        
+        o = epicells( i );
+        [ f, v  ] = fit_ellipse_3d( double( o.boundary ) );
+        
+        hc( i ) = o.plot_contour_3d;
+        he( i ) =  plot_ellipse_3d( f, v );
+    end
+
+    add_plot_scalebar( gca,  epicells, 10, 'µm' );
+
+end
+
